38e67ece5f3a77d02b5094647ef4f389f24dede1
Przemek.Konstanczuk <Przemek.Konstanczuk@c06c485e-02b2-11df-897e-95fb7fca2173>
2010-04-08 18:34:24 +0000

    pierwszy dawn Detector

---
+++ DawnDetector/DawnDetectorForm.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace DawnDetector
+{
+    partial class DawnDetector
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dawnTimer = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dawnTimer
+            // 
+            this.dawnTimer.Interval = 1000;
+            this.dawnTimer.Tick += new System.EventHandler(this.dawnTimer_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::DawnDetector.Properties.Resources.IMG_0238;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 131);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.CustomFormat = "HH:MM";
+            this.dateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker.Location = new System.Drawing.Point(13, 161);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker.TabIndex = 2;
+            // 
+            // DawnDetector
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(230, 197);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "DawnDetector";
+            this.Text = "Dawn Detector";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer dawnTimer;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+    }
+}
+
