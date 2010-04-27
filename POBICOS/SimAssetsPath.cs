da62bf854855476a03c9954bf59bee60da1bfdaf
yacek.milewski <yacek.milewski@c06c485e-02b2-11df-897e-95fb7fca2173>
2010-04-27 12:40:10 +0000

    zmiana wielkości tekstury dla kart graficznych starszego typu

--- POBICOS/SimAssetsPath.cs
+++ POBICOS/SimAssetsPath.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace POBICOS
+﻿namespace POBICOS
 {
 	public static class SimAssetsPath
 	{
-		public static string MODELS_PATH = "Models/";
-		public static string MODELS_BUILDING_PATH = "Building/";
+		public static string MODELS_PATH = @"Models/";
+		public static string MODELS_BUILDING_PATH = @"Building/";
         public static string MODELS_ENVIRONMENT_PATH = @"Building/Environment/";
-		public static string EFFECTS_PATH = "Effects/";
-        public static string POBICOS_OBJECTS_PATH = @"..\..\..\..\XML\";
+		public static string EFFECTS_PATH = @"Effects/";
+        public static string POBICOS_OBJECTS_PATH = @"./../../../../XML/";
 	}
 }
